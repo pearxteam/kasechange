0da98ea64038548699f8787dde88dd6ecee2e69a
mrAppleXZ <mr.applexz@gmail.com>
2019-05-25 23:14:59 +0500

    Added the copyright notice

--- src/commonMain/kotlin/net/pearx/kasechange/StringExtensions.kt
+++ src/commonMain/kotlin/net/pearx/kasechange/StringExtensions.kt
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2019, PearX Team
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package net.pearx.kasechange
 
 fun String.toCase(format: CaseFormat) = format.format(splitToWords())
