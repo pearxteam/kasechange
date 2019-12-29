6c59745dcb2cdb4f572638dec0bbd984355978fd
mrAppleXZ <mr.applexz@gmail.com>
2019-12-29 15:16:51 +0500

    Update copyrights

--- src/jsMain/kotlin/net/pearx/kasechange/CharUtils.kt
+++ src/jsMain/kotlin/net/pearx/kasechange/CharUtils.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2019, PearX Team
+ * Copyright © 2019-2020, PearX Team
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/.
